178b03632cc0bbe7effb2f774400d3a03c822f19
octagonn <79621686+octagonn@users.noreply.github.com>
2025-05-03 00:39:25 -0700

    google reviews logo

--- components/reviews.php
+++ components/reviews.php
@@ -3,7 +3,7 @@
         <h2><span class="icon-yellow">⭐</span> Leave A Google Review!</h2>
         <p class="review-text">Help us grow by sharing your experience with others.</p>
         <a href="https://g.page/r/Ca4pUoaLIJ6MEAE/review" target="_blank" class="review-link">
-            <img src="https://www.freepngimg.com/thumb/google/66530-engine-web-search-google-now-voice.png" alt="Google" class="yelp-icon">
+            <img src="img/google-logo.png" alt="Google" class="review-logo" style="max-width: 150px; height: auto;">
             <span class="review-button">Write a Review</span>
         </a>
     </div>
